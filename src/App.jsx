d8fa7222d3d7d6e6cd36f84bc85dc9236e49b835
Satyam Rajput <satyamrajput081998@gmail.com>
2024-11-16 10:36:03 -0500

    More Input Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,62 @@
+import { useState } from 'react'
 import './App.css'
+import Input from './input/input'
 
 function App() {
+  const [value,setValue] = useState('')
   return (
     <>
-    <h2>Hello My New Branch</h2>
+      <h3>Normal type Input</h3>
+      <div className='input-container'>
+        <Input placeholder={"Basic usage"} className={"simple-input"} type={"text"} onchange={(e) => setValue(e.target.value)} />
+      </div>
+      <div className='input-container'>
+        <Input placeholder={"Fill your Password"} className={"password-input"} type={"password"} />
+      </div>
+      <div className='input-container'>
+        <Input placeholder={"Fill your Email Id"} className={"email-input"} type={"email"} />
+      </div>
+      <div className='input-container'>
+        <Input placeholder={"Please Enter a Number"} className={"number-input"} type={"number"} />
+      </div>
+      <div className='twotype-inputContainer'>
+        <Input className={"small-input"} /><Input className={"medium-input"}/>
+      </div>
+      <div className='threetype-inputConatiner'>
+        <Input type={"search"} className={"inner"} placeholder={"Large-Size"} /> <Input className={"outer"} placeholder={"Another-Size"}/>
+      </div>
+
+      <div className='threetype-inputConatiner'>
+        <Input type={"search"} className={"innersecond-com"} placeholder={"Large-Size"} value={"https://"}/> <Input className={"outersecond-com"} placeholder={"Another-Size"}/>
+      </div>
+      <div className='input-container'>
+        <Input type={"spinner"} className={"simple-input"} placeholder={"input Search Loading default"}/>
+      </div>
+      <div className='inputSpinnerbtn-container'>
+          <Input type={"spinnerbutton"} className={"simple-input"} placeholder={"input Search Loading default"}/>
+      </div>
+
+      <div className='inputSpinner-container'>
+        <Input type={'spinnerButtonText'} placeholder={"Search....."} />
+      </div>
+
+      <div className='btnsearch'>
+        <Input type={"searchbtn"} placeholder={"Search"}/>
+      </div>
+
+      <div className='mictype-conatiner'>
+          <Input type={"mic"} placeholder={"Search with Mic"}/>
+      </div>
+
+      <div className='passwordIcon-container'>
+        <Input type={"passwordIcon"} placeholder={"Input Password"}/>
+      </div>   
+
+      <div className='showpassword-container'>
+          <Input type={"showPassword"} placeholder={"Fill YOUR PASSWORD"}/>
+      </div>
+  
+      <p>{value}</p>  
     </>
   )
 }
