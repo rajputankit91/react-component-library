6492ad434d886c96520e4574a111ecb1fa14998c
Ankit Kumar <ankitrajput10791@gmail.com>
2024-11-18 22:26:20 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Image from './image component/Image'
-import ImageComponent from './image component/Image'
 
 function App() {
 
   return (
     <>
-      <ImageComponent
+      <Image
         src="https://png.pngtree.com/thumb_back/fh260/background/20240327/pngtree-3d-hindu-god-ganpati-image-with-green-nature-background-image_15695930.jpg"
         alt="Example"
-        variant="circle"
+        variant="rounded"
         zoom={true}
       />
 
