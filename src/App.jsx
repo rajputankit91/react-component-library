65134a5d2c47699299b4953739a1a2bc8c7b99a9
Ankit Kumar <ankitrajput10791@gmail.com>
2024-11-18 22:14:17 +0530

    create image component

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Image from './image component/Image'
+import ImageComponent from './image component/Image'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ImageComponent
+        src="https://png.pngtree.com/thumb_back/fh260/background/20240327/pngtree-3d-hindu-god-ganpati-image-with-green-nature-background-image_15695930.jpg"
+        alt="Example"
+        variant="circle"
+        zoom={true}
+      />
+
     </>
   )
 }
