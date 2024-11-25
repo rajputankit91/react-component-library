dea11a49dec343d9550021a40bb149181b210df6
Nandiniatri <atrinandini8@gmail.com>
2024-11-25 14:54:40 +0530

    add Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Button from './Components/button'
 import { IoIosSearch } from "react-icons/io";
+import { FaSpinner } from "react-icons/fa";
+import { MdOutlineFileDownload } from "react-icons/md";
 
 
 function App() {
@@ -11,38 +13,41 @@ function App() {
 
   return (
     <div className='button-main-container'>
-      <div className='div1'>
+      <div>
         <h3>Example Of Button</h3>
-        <Button className="Default" type="default">Default Button</Button>
-        <Button className="Primary" type="primary">Primary Button</Button>
-        <Button className="dashed" type='dashed'>Dashed Button</Button>
-        <Button className="text" type='text'>Text Button</Button>
-        <Button className="link" type='link'>Link Button</Button>
+        <div className='Examplebutton'>
+          <Button className="Default" variant='default'>Default Button</Button>
+          <Button className="Primary" variant="primary">Primary Button</Button>
+          <Button className="dashed" variant='dashed'>Dashed Button</Button>
+          <Button className="text" variant='text'>Text Button</Button>
+          <Button className="link" variant='link'>Link Button</Button>
+        </div>
       </div>
-      
+
       <div>
         <h3>Button Event</h3>
         <Button className="Default" type='default' onClick={handleButton}>Click Button</Button>
       </div>
+
       <div>
         <h3>Button with Color</h3>
-        <div>
-          <Button className="solid" type='primary'>Solid</Button>
+        <div className='exampleBtnWithColor'>
+          <Button className="solid" variant='solid'>Solid</Button>
           <Button className="outlined" type='outlined'>Outlined</Button>
           <Button className="dashed" type='dashed'>Dashed</Button>
           <Button className="filled" type='filled'>Filled</Button>
           <Button className="text" type='text'>Text</Button>
           <Button className="link" type='link'>Link</Button>
         </div>
-        <div>
-          <Button className="solid-1" type='primary'>Solid</Button>
+        <div className='exampleBtnWithColor'>
+          <Button className="solid-1" variant="primary">Solid</Button>
           <Button className="outlined-1" type='outlined'>Outlined</Button>
           <Button className="dashed-1" type='dashed'>Dashed</Button>
           <Button className="filled-1" type='filled'>Filled</Button>
           <Button className="text-1" type='text'>Text</Button>
           <Button className="link-1" type='link'>Link</Button>
         </div>
-        <div>
+        <div className='exampleBtnWithColor'>
           <Button className="solid-2" type='primary'>Solid</Button>
           <Button className="outlined-2" type='outlined'>Outlined</Button>
           <Button className="dashed-2" type='dashed'>Dashed</Button>
@@ -57,33 +62,58 @@ function App() {
         <div className='buttonIcons'>
           <Button className="search" type="search" icon={<IoIosSearch />}></Button>
           <Button className="A" type="primary">A</Button>
-          <Button icon={<IoIosSearch />} type="search"  >Search</Button>
-          <Button icon={<IoIosSearch />} type="search"></Button>
-          <Button icon={<IoIosSearch />} type="search">Search</Button>
-          <Button icon={<IoIosSearch />} type="search"></Button>
-          <Button icon={<IoIosSearch />} type="search">Search</Button>
-          <Button icon={<IoIosSearch />} type="search"></Button>
-          <Button icon={<IoIosSearch />} type="search">Search</Button>
-          <Button icon={<IoIosSearch />} type="search"></Button>
+          <Button className="search1" icon={<IoIosSearch />} type="search">Search</Button>
+          <Button className="search2" icon={<IoIosSearch />} type="search"></Button>
+          <Button className="search3" icon={<IoIosSearch />} type="search" >Search</Button>
+          <Button className="search4" icon={<IoIosSearch />} type="search">Search</Button>
+          <Button className="search5" icon={<IoIosSearch />} type="search"></Button>
+          <Button className="search6" icon={<IoIosSearch />} type="search">Search</Button>
         </div>
       </div>
 
       <div>
         <h3>Button With Positions Icon</h3>
-        <div className='buttonIcons'>
-          <Button className="search" type="search" icon={<IoIosSearch />}></Button>
+        <div className="buttonIcons">
+          <Button className="search" type="search" icon={<IoIosSearch />} />
           <Button className="A" type="primary">A</Button>
-          <Button icon={<IoIosSearch />} type="searchBack" >Search</Button>
-          <Button icon={<IoIosSearch />} type="searchBack" ></Button>
-          <Button icon={<IoIosSearch />} type="searchBack" >Search</Button>
-          <Button icon={<IoIosSearch />} type="searchBack" ></Button>
-          <Button icon={<IoIosSearch />} type="searchBack" >Search</Button>
-          <Button icon={<IoIosSearch />} type="searchBack" ></Button>
-          <Button icon={<IoIosSearch />} type="searchBack" >Search</Button>
-          <Button icon={<IoIosSearch />} type="searchBack" ></Button>
+          <Button className="search1" icon={<IoIosSearch />} type="searchBack">Search</Button>
+          <Button className="search2" icon={<IoIosSearch />} type="searchBack" />
+          <Button className="search3" icon={<IoIosSearch />} type="searchBack">Search</Button>
+          <Button className="search4" icon={<IoIosSearch />} type="searchBack" >Search</Button>
+          <Button className="search5" icon={<IoIosSearch />} type="searchBack" />
+          <Button className="search6" icon={<IoIosSearch />} type="searchBack">Search</Button>
         </div>
       </div>
 
+
+      <div>
+        <h3>Button with Loading Icon</h3>
+        <div>
+          <Button className="search3" type="searchBack" icon={<FaSpinner />}>Loading</Button>
+        </div>
+      </div>
+
+      <div>
+        <h3>Button with Downloading</h3>
+        <div className='btnDownLoad'>
+          <Button className="search" type="search" icon={<MdOutlineFileDownload />} />
+          <Button type="search" icon={<MdOutlineFileDownload />} />
+          <Button type="search" icon={<MdOutlineFileDownload />} />
+          <Button type="search" icon={<MdOutlineFileDownload />}>
+            Download
+          </Button>
+          <Button className="search1 " type="search" icon={<MdOutlineFileDownload />}>
+            Download
+          </Button>
+        </div>
+      </div>
+
+      <div>
+        <h3>Button with Disable</h3>
+        <div className='btnDisable'>
+          <Button className="search3" disabled={true}>Hello</Button>
+        </div>
+      </div>
 
     </div>
   )
--- src/Components/button.jsx
+++ src/Components/button.jsx
@@ -1,22 +1,147 @@
 import './button.css';
 
-const Button = ({ children, className, type, onClick, icon }) => {
+const Button = ({ children, className, type, onClick, icon, variant , size , disabled='false'}) => {
+
+
+    const solidClass = {
+        border: 'none',
+        backgroundColor: 'black', 
+        color: 'white', 
+        width: '130px', 
+        height: '45px', 
+        borderRadius: '5px', 
+        fontSize: '14px',
+        cursor: 'pointer'
+    };
+
+    const outlinedClass = {
+        backgroundColor: '#ffffff',
+        color: 'black',
+        width: '130px',
+        height: '45px',
+        borderRadius: '5px',
+        fontSize: '14px',
+        cursor: 'pointer',
+        border: 'solid 1px gray'
+    };
+
+    const dashedClass = {
+        backgroundColor: '#ffffff',
+        color: 'black',
+        width: '130px',
+        height: '45px',
+        borderRadius: '5px',
+        fontSize: '14px',
+        cursor: 'pointer',
+        border: '1px solid gray',
+        borderTop: '1px dotted gray',
+        borderLeft: '1px dotted gray',
+        borderRight: '1px dotted gray',
+        borderBottom: '1px dotted gray'
+    };
+
+    const filledClass = {
+        backgroundColor: '#efeded',
+        color: 'black',
+        width: '130px',
+        height: '45px',
+        borderRadius: '5px',
+        fontSize: '14px',
+        cursor: 'pointer',
+        border: 'none'
+    };
+
+
+    const textClass = {
+        border: 'none',
+        backgroundColor: '#ffffff',
+        color: 'black',
+        width: '130px',
+        height: '45px',
+        borderRadius: '5px',
+        fontSize: '14px',
+        cursor: 'pointer',
+        transition: 'all 0.3s ease'
+    };
+
+
+    const linkClass = {
+        border: 'none',
+        backgroundColor: '#ffffff',
+        color: 'black', 
+        width: '130px', 
+        height: '45px',
+        borderRadius: '5px',
+        fontSize: '14px', 
+        cursor: 'pointer',
+        transition: 'all 0.3s ease' 
+    };
+    
+
+    const defaultClass = {
+        border: '1px solid gray',
+        backgroundColor: '#ffffff',
+        color: 'black',
+        width: '130px',
+        height: '45px',
+        borderRadius: '5px',
+        fontSize: '14px',
+        cursor: 'pointer',
+        transition: 'all 0.3s ease'
+    };
+
+    const primaryClass = {
+        border: 'none',
+        backgroundColor: '#1677FF',
+        color: 'white',
+        width: '130px',
+        height: '45px',
+        borderRadius: '5px',
+        fontSize: '14px',
+        cursor: 'pointer',
+        transition: 'all 0.3s ease'
+    };
 
     var iconElement = null;
     var iconElementBack = null;
 
+    let btnStyle = {};
+    // let sizeStyle = 
+
+    if (variant === 'solid') {
+        btnStyle = solidClass;
+    } else if (variant === 'outlined') {
+        btnStyle = outlinedClass;
+    } else if (variant === 'dashed') {
+        btnStyle = dashedClass;
+    } else if (variant === 'filled') {
+        btnStyle = filledClass;
+    } else if (variant === 'text') {
+        btnStyle = textClass;
+    } else if (variant === 'link') {
+        btnStyle = linkClass;
+    } else if (variant === 'default') {
+        btnStyle = defaultClass;
+    } else if (variant === 'primary') {
+        btnStyle = primaryClass;
+    } 
+
+    // console.log(btnStyle);
+
+
     if (type === 'search') {
         iconElement = icon;
-    } else if (type === 'default') {
-        iconElement = '';
-    }else if(type === 'primary'){
-        iconElement = ''
-    }else if(type === 'searchBack'){
+    } else if (type === 'searchBack') {
         iconElementBack = icon;
+    } else if(type === 'loader'){
+        iconElement = icon;
+    }else if(type === 'download'){
+        iconElement = icon;
     }
 
+
     return (
-        <button className={className} type={type} onClick={onClick}>
+        <button className={className} type={type} onClick={onClick} style={btnStyle} size={size} disabled={disabled} >
             <span className='icon-text-container'>
                 {iconElement}
                 {children}
